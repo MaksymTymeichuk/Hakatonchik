3e3fc6d537d62870acb298380088b7d0756d2614
MaksymTymeichuk <max.tymeychuk@gmail.com>
2025-10-15 10:30:59 +0300

    Add Student function

--- studenthelper.cpp
+++ studenthelper.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
+#include <map>
 using namespace std;
 
+
+map<string, map<string, map<string, int>>> add_student(string name, string group, map<string, map<string, map<string, int>>> students) {
+    students[group][name] = {
+        {"Дискретна математика", 0},
+        {"Математичний аналіз", 0},
+        {"Фізкультура", 0}
+    };
+
+
+    return students;
+};
+
 int main(){
-    cout <<"Hello ok";
-    cout << "aaaaaaaaaaa";
+
     return 0;
 }
+
